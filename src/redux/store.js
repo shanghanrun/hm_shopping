f2a3fd65bb205bfd84c10bcba18f3176597b0b82
shanghanrun <idim7@naver.com>
2024-03-11 14:44:18 +0900

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import {createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from '@redux-devtools/extension';
 // import thunk from 'redux-thunk'
 import {thunk} from 'redux-thunk'
 import rootReducer from './reducers'; // ./reducers/index 인데,   폴더 이름만 해주면 그 안의 index.js파일을 자동으로 읽어온다.
 
-let store = createStore(rootReducer, applyMiddleware(thunk))
+let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
